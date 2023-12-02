65b422163e1dfe9ffcc11e59e347a2dc54df72bb
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-12-01 19:58:00 -0500

    #a little of this

---
+++ index.js
@@ -0,0 +1,60 @@
+
+let currentMoleTile;
+let currentPlantTile;
+
+
+window.onload = function() {
+  setGame();
+};
+
+function setGame() {
+  for (let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+    tile.id = i.toString();
+    document.getElementById("board").appendChild(tile);
+  }
+
+  setInterval(setMole, 1000);
+setInterval(setPlant, 2000);
+
+  }
+function getRandomTile() {
+  let num = Math.floor(Math.random() * 9);
+  return num.toString();
+}
+  function setMole() {
+
+    if (currentMoleTile) {
+      currentMoleTile.innerHTML = "";
+    }
+
+let mole = document.createElement("img");
+mole.src = "./monty-mole.png";
+
+let num = getRandomTile();
+if (currentPlantTile && currentPlantTile.id === num) {
+  return;
+}
+
+currentMoleTile = document.getElementById(num);
+currentMoleTile.appendChild(mole);
+
+  }
+
+  function setPlant() {
+
+    if (currentPlantTile) {
+      currentPlantTile.innerHTML = "";
+    }
+
+let plant = document.createElement("img");
+plant.src = "./piranha-plant.png";
+
+let num = getRandomTile();
+if (currentMoleTile && currentMoleTile.id === num) {
+  return;
+}
+currentPlantTile = document.getElementById(num);
+currentPlantTile.appendChild(plant);
+
+  }
